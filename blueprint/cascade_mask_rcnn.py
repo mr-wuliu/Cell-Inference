eeda80c3c50810d604acbfdae5bf6982ce89a2c1
Tairitsu <1026656264@qq.com>
2023-07-12 20:08:26 +0800

    1. fix bug of pr_curve that couldn't skip ahead to other pages when click event happened. 2. add a nav-button on nav-bar in common/base.html

--- blueprint/cascade_mask_rcnn.py
+++ blueprint/cascade_mask_rcnn.py
@@ -15,6 +15,7 @@ bp = Blueprint('cascade_mask_rcnn', __name__)
 processes = {}
 current_key = ''
 
+
 # 定义单个模型的路由
 class model:
     home = 'cascade_mask_rcnn.home'
@@ -282,27 +283,19 @@ def result():
                            t_sne=t_sne, loss_title='Cascade Mask R-CNN Loss',
                            model=model)
 
+
 # @bp.route('/matrix', methods=['GET'])
 @bp.route('/matrix')
 def matrix():
-
-    #  # 初始化 confusion_matrix_file  判断是否有图片生成
-    # confusion_matrix_file = ''
-
-    # # 调用函数
-    # result = calculate_confusion_matrix(config_file, dateload_file, save_dir)
-
-    # # 检查调用结果
-    # if result is not None:
-    #     output = 'Confusion matrix calculated successfully.'
-    #     confusion_matrix_file = os.path.join(save_dir, 'matrix.png')
-    # else:
-    #     output = 'Error calculating confusion matrix.'
-    #     confusion_matrix_file = ''
-
+    script_name = model.home
+    if script_name.startswith("cascade"):
+        script_name = "cascade"
+    model_name = script_name.split('.')[0]
+    file_name = 'img/confusion_matrix/' + model_name + '/confusion_matrix.png'
     # # 将结果传递给模板进行渲染
-    # return render_template('mask_rcnn/matrix.html',output=output,confusion_matrix_file=confusion_matrix_file, model=model)
-    return render_template('cascade_mask_rcnn/matrix.html', model=model)
+    return render_template('cascade_mask_rcnn/matrix.html', model=model,
+                           file_name=file_name)
+
 
 """
 接口请求
